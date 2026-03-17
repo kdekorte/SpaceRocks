c940682db1af2a8d84b77e7bf66ae87559830db3
Kevin DeKorte <kdekorte@yahoo.com>
2026-03-17 13:54:38 -0600

    Initial Commit

---
+++ SpaceRocks/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SpaceRocks
+//
+//  Created by Kevin DeKorte on 3/17/26.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
