f950a9842fa092fb1100a2a5569728a0ff9460bc
Kevin DeKorte <kdekorte@gmail.com>
2026-07-28 07:55:41 -0600

    Updated for release

--- Casks/spacerocks.rb
+++ Casks/spacerocks.rb
@@ -1,8 +1,8 @@
 cask "spacerocks" do
-  version "1.0.0"
-  sha256 "<SHA256_PLACEHOLDER>"
+  version "v1.0.0"
+  sha256 "015eb8b68455500e24a2c68dc06ecca9574aca642dda7e459f950ba5009f1a7e"
 
-  url "https://github.com/kdekorte/SpaceRocks/releases/download/v#{version}/SpaceRocks.zip"
+  url "https://github.com/kdekorte/SpaceRocks/releases/download/v1.0.0/SpaceRocks.zip"
   name "SpaceRocks"
   desc "Classic arcade-style asteroid shooter for macOS"
   homepage "https://github.com/kdekorte/SpaceRocks"
